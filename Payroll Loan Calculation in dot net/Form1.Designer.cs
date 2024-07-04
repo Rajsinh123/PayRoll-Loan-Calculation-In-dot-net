4460c191445aeeaea705b855c9731303c99d145b
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-07-04 11:54:57 +0530

    Add project files.

---
+++ Payroll Loan Calculation in dot net/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Payroll_Loan_Calculation_in_dot_net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textInterset = new System.Windows.Forms.TextBox();
+            this.textMonth = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textAmount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtAm = new System.Windows.Forms.TextBox();
+            this.txtMpay = new System.Windows.Forms.TextBox();
+            this.txtTot = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lime;
+            this.label5.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(336, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "PayRoll Loan";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textInterset);
+            this.panel2.Controls.Add(this.textMonth);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textAmount);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(72, 96);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(355, 221);
+            this.panel2.TabIndex = 1;
+            // 
+            // textInterset
+            // 
+            this.textInterset.ForeColor = System.Drawing.Color.Purple;
+            this.textInterset.Location = new System.Drawing.Point(154, 145);
+            this.textInterset.Name = "textInterset";
+            this.textInterset.Size = new System.Drawing.Size(100, 20);
+            this.textInterset.TabIndex = 5;
+            // 
+            // textMonth
+            // 
+            this.textMonth.ForeColor = System.Drawing.Color.Purple;
+            this.textMonth.Location = new System.Drawing.Point(154, 92);
+            this.textMonth.Name = "textMonth";
+            this.textMonth.Size = new System.Drawing.Size(100, 20);
+            this.textMonth.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(38, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Interset";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(38, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Month";
+            // 
+            // textAmount
+            // 
+            this.textAmount.ForeColor = System.Drawing.Color.Purple;
+            this.textAmount.Location = new System.Drawing.Point(154, 40);
+            this.textAmount.Name = "textAmount";
+            this.textAmount.Size = new System.Drawing.Size(100, 20);
+            this.textAmount.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(38, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Amount";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(87, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(484, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 16);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Amount";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(464, 172);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 16);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Month Payment";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(484, 230);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 16);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Total";
+            // 
+            // txtAm
+            // 
+            this.txtAm.Location = new System.Drawing.Point(657, 109);
+            this.txtAm.Name = "txtAm";
+            this.txtAm.Size = new System.Drawing.Size(100, 20);
+            this.txtAm.TabIndex = 6;
+            // 
+            // txtMpay
+            // 
+            this.txtMpay.Location = new System.Drawing.Point(657, 168);
+            this.txtMpay.Name = "txtMpay";
+            this.txtMpay.Size = new System.Drawing.Size(100, 20);
+            this.txtMpay.TabIndex = 7;
+            // 
+            // txtTot
+            // 
+            this.txtTot.Location = new System.Drawing.Point(657, 242);
+            this.txtTot.Name = "txtTot";
+            this.txtTot.Size = new System.Drawing.Size(100, 20);
+            this.txtTot.TabIndex = 8;
+            this.txtTot.TextChanged += new System.EventHandler(this.txtTot_TextChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(997, 371);
+            this.Controls.Add(this.txtTot);
+            this.Controls.Add(this.txtMpay);
+            this.Controls.Add(this.txtAm);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "PayRoll";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textInterset;
+        private System.Windows.Forms.TextBox textMonth;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textAmount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtAm;
+        private System.Windows.Forms.TextBox txtMpay;
+        private System.Windows.Forms.TextBox txtTot;
+    }
+}
+
---
+++ Payroll Loan Calculation in dot net/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Payroll_Loan_Calculation_in_dot_net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtTot_TextChanged(object sender, EventArgs e)
+       {
+       }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double amount, interset,monthpayment;
+            int month;
+            amount = double.Parse(textAmount.Text);
+            month = int.Parse(textMonth.Text);
+
+            interset = double.Parse(textInterset.Text);
+            monthpayment = amount * interset / month;
+            double fullpayment = monthpayment / month;
+
+            txtAm.Text = amount.ToString("C2",CultureInfo.CurrentCulture);
+            txtMpay.Text = monthpayment.ToString("C2", CultureInfo.CurrentCulture);
+            txtTot.Text = fullpayment.ToString("C2", CultureInfo.CurrentCulture);
+
+        }
+    }
+}
